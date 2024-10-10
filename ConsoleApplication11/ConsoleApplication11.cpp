49a28cf0359f65fb102c7f2757581e085507d047
Fraps <Fraps@NOUTDON_ZHI_EST>
2024-10-11 00:29:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+class MyClass {
+public:
+    int data;
+
+    void printData() {
+        std::cout << data << std::endl; // Прямой доступ
+    }
+};
+
+int main() {
+    MyClass obj;
+    obj.data = 20;
+    obj.printData(); // Вывод: 20
+    return 0;
+}
